677932ce618618693b9cff6e4ac9d1ce14e76a95
lgz <1429277983@qq.com>
2020-04-06 11:24:31 +0800

    加网页图标

--- src/main/java/com/lgz/util/MD5Utils.java
+++ src/main/java/com/lgz/util/MD5Utils.java
@@ -35,7 +35,7 @@ public class MD5Utils {
     }
 
 //    public static void main(String[] args) {
-//        System.out.println(code("111111"));
+//        System.out.println(code("116118651"));
 //    }
 
 }
